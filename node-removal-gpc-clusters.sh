a3bbbb8c2edb6f3938ceb2ccdee06ca783d6cd0c
cpetee <47620770+cpetee@users.noreply.github.com>
2026-04-22 23:08:35 -0400

    Update node-removal-gpc-clusters.sh
    update to include variable redis instance and update node# to input values

--- node-removal-gpc-clusters.sh
+++ node-removal-gpc-clusters.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 ##cpetee april 2026
-## help from AI
+## help from AI (gemini) from initial script/template)
+
 #!/bin/bash
 
 # Function to prompt for input with a default value
 prompt_var() {
-    local var_name=$1
     local prompt_text=$2
     local default_value=$3
     read -p "$prompt_text [$default_value]: " input
@@ -15,159 +16,188 @@ echo "------------------------------------------"
 echo "   GPC Node Removal Worklog Generator     "
 echo "------------------------------------------"
 
-# Interactive Prompts
-CLUSTER_PREFIX=$(prompt_var "CLUSTER_PREFIX" "Enter Cluster Prefix" "example-cluster")
-SERVER_LIST=$(prompt_var "SERVER_LIST" "Enter Server List (e.g. node3, node4)" "node3, node4")
-CASE_ID=$(prompt_var "CASE_ID" "Enter Case ID" "0000000")
-CLIENT_ID=$(prompt_var "CLIENT_ID" "Enter Client ID" "12345")
-REDIS_INSTANCE1=$(prompt_var "REDIS_INSTANCE1" "Enter Redis Instance 1" "inst1")
-REDIS_INSTANCE2=$(prompt_var "REDIS_INSTANCE2" "Enter Redis Instance 2" "inst2")
-INTERNAL_IP_FS1=$(prompt_var "INTERNAL_IP_FS1" "Enter FS1 Internal IP" "10.0.0.1")
-INTERNAL_IP_NODES_REMOVED=$(prompt_var "INTERNAL_IP_NODES_REMOVED" "Enter Removed Nodes IP Range" "10.0.0.[x-y]")
+# 1. Basic Info
+CLUSTER_PREFIX=$(prompt_var "" "Enter Cluster Prefix" "example-cluster")
+CASE_ID=$(prompt_var "" "Enter Case ID" "0000000")
+CLIENT_ID=$(prompt_var "" "Enter Client ID" "12345")
+CLIENT_TEAM=$(prompt_var "" "Enter Client Team Name" "ClientName")
+INTERNAL_IP_FS1=$(prompt_var "" "Enter FS1 Internal IP" "10.0.0.1")
+INTERNAL_IP_NODES_REMOVED=$(prompt_var "" "Enter Removed Nodes IP Range" "10.0.0.[x-y]")
 
-# Domain Array Logic
-read -p "Enter Domains (separated by spaces) [example.com]: " DOMAIN_INPUT
-DOMAIN_INPUT=${DOMAIN_INPUT:-"example.com"}
-# Convert input string into a proper array
+# 2. Server List Logic
+read -p "Enter Nodes (separated by spaces, e.g., node5 node6): " NODE_INPUT
+read -a NODE_ARRAY <<< "$NODE_INPUT"
+# Create formatted versions
+NODE_BULLET_LIST=$(printf -- "- ${CLUSTER_PREFIX}-%s.us-midwest-1.nxcli.net\n" "${NODE_ARRAY[@]}")
+NODE_COMMA_LIST=$(printf ", %s" "${NODE_ARRAY[@]}" | cut -c 3-)
+NODE_BLACKLIST=$(printf "${CLUSTER_PREFIX}-%s.us-midwest-1.nxcli.net\n" "${NODE_ARRAY[@]}")
+NODE_INT_LIST=$(printf "${CLUSTER_PREFIX}-%s-int\n" "${NODE_ARRAY[@]}")
+
+# 3. Redis Instance Logic (Dynamic Array)
+read -p "Enter Redis Instances (separated by spaces, e.g., inst1 inst2): " REDIS_INPUT
+read -a REDIS_ARRAY <<< "$REDIS_INPUT"
+
+REDIS_STOP_BLOCK=""
+REDIS_STATUS_BLOCK=""
+REDIS_RESET_BLOCK=""
+
+for inst in "${REDIS_ARRAY[@]}"; do
+    REDIS_STOP_BLOCK+=$'systemctl stop redis-multi-'"$inst"$'-replica.service\n'
+    REDIS_STATUS_BLOCK+=$'systemctl status redis-multi-'"$inst"$'-replica.service\n'
+    REDIS_RESET_BLOCK+=$'SENTINEL RESET '"$inst"$'-cache\n'
+done
+
+# 4. Domain Logic
+read -p "Enter Domains (separated by spaces): " DOMAIN_INPUT
 read -a DOMAIN_ARRAY <<< "$DOMAIN_INPUT"
-# Format array into a newline-separated string
 DOMAIN_LIST=$(printf "%s\n" "${DOMAIN_ARRAY[@]}")
 
 echo -e "\n--- Generating Document ---\n"
 
 # The Template
 TEMPLATE=$(cat << 'EOF'
-Waypoint reference: https://waypoint.liquidweb.com:8443/display/SUP/GPC+Node+Removals
-target cluster: <CLUSTER PREFIX>
-target server to be removed: <SERVER LIST>
-Support case: <CASE ID>
-Client account: https://nocworx.nexcess.net/client/<CLIENT ID>
- 
-Steps to following
-update client in case:  <CASE ID>
+WEB <NODE_COMMA_LIST> REMOVAL
+--------------------
+Nodes to remove:
+<NODE_BULLET_LIST>
 
-also update OCC team in #nw-watchers
+Status  |  Step  |  Action                                                                            |  Who
+-----------------------------------------------------------------------------------------------------------------------------------------------
+[ ]     |  0)    |  Update client in case and update OCC in #nw-watchers                              |  ( Nexcess )
+[ ]     |  1)    |  Merge Inventory blacklist                                                         |  ( Nexcess )
+[ ]     |  2)    |  Pause Status Cake                                                                 |  ( Nexcess )
+[ ]     |  3)    |  Regenerate the Ansible inventory (perform on esg-ansible)                         |  ( Nexcess )
+[ ]     |  4)    |  Backup HAProxy and Varnish configuration                                          |  ( Nexcess )
+[ ]     |  5)    |  Enable maintenance mode on the sites via HAProxy                                  |  ( Nexcess )
+[ ]     |  6)    |  Disable local document roots (on esg-ansible)                                     |  ( Nexcess )
+[ ]     |  7)    |  Run Ansible playbooks (on esg-ansible)                                            |  ( Nexcess )
+[ ]     |  8)    |  Enable local docroots (on esg-ansible)                                            |  ( Nexcess )
+[ ]     |  9)    |  !! THIS IS THE MOST IMPORTANT STEP !! Drop the nodes from Interworx (on <CLUSTER PREFIX>-lb1) |  ( Nexcess )
+[ ]     |  10)   |  Allow access from your IP addresss and perform simple testing                     |  ( Nexcess )
+[ ]     |  11)   |  Verify that no traffic is routing to the old nodes                                |  ( Nexcess )
+[ ]     |  12)   |  Assist the client in testing the site                                             |  ( Nexcess/<CLIENT_TEAM>)
+[ ]     |  13)   |  Remove maintenance                                                                |  ( Nexcess )
+[ ]     |  14)   |  Update OCC team in #nw-watchers                                                   |  ( Nexcess )
+[ ]     |  15)   |  Re-enable Status Cake alert                                                       |  ( Nexcess )
+[ ]     |  16)   |  Pass back to ESG Projects for decommissioning <NODE_COMMA_LIST>                   |  ( Nexcess )
+
+Notes by step:
+--------------
+0) Update client in case: <CASE ID>
+- Also update OCC team in #nw-watchers
 Please ignore alerts for  <CLUSTER PREFIX>-* and scheduled maintenance is being worked on.
-update Inventory blacklist
 
+1) Update and Merge Inventory blacklist
 add following to inventories/blacklist.txt in ansible
-<CLUSTER PREFIX>-node#.us-midwest-1.nxcli.net
-<CLUSTER PREFIX>-node#.us-midwest-1.nxcli.net
-Pause status cake
+<NODE_BLACKLIST>
+- Verify the following is in inventories/blacklist.txt in ansible
+<NODE_BLACKLIST>
 
+2) Pause status cake
 https://app.statuscake.com/Login/?redirect=/YourStatus2.php
-Pause Statuscake monitoring for the server(s) being removed
+- Pause Statuscake monitoring for the server(s) being removed
 Use the "NX-SOS Fleet StatusCake [rmacdonaldnexcessnet]" entry in Bitwarden
 Search for the hostnames
 Hit the pause button on the test (if a resume button is seen instead, that means that the test is already paused)
-Regenerate the Ansible inventory (perform on esg-ansible)
 
+3) Regenerate the Ansible inventory (perform on esg-ansible)
 sudo /usr/local/sbin/getinv
-Take a backup of the HAProxy and Varnish configuration (perform on <CLUSTER PREFIX>-lb1.us-midwest-1.nxcli.net )
-rsync -aAXvP /etc/haproxy/ "/etc/haproxy-<CASE ID>-$(date --iso-8601=minute)"
 
+4) Backup HAProxy and Varnish configuration (perform on <CLUSTER PREFIX>-lb1.us-midwest-1.nxcli.net )
+rsync -aAXvP /etc/haproxy/ "/etc/haproxy-<CASE ID>-$(date --iso-8601=minute)"
 rsync -aAXvP /etc/varnish/ "/etc/varnish-<CASE ID>-$(date --iso-8601=minute)"
 
-Enable maintenance mode on the sites via HAProxy (performed on <CLUSTER PREFIX>-lb1)
-Edit /etc/haproxy/lists/maintenance-domains.txt and add:
-
+5) Enable maintenance mode on the sites via HAProxy (performed on <CLUSTER PREFIX>-lb1)
+- Edit /etc/haproxy/lists/maintenance-domains.txt and add:
 <DOMAINS>
-
-Reload HAProxy
-
+- Reload HAProxy
 haproxy -c -V -f /etc/haproxy/conf/ -f /etc/haproxy/conf.d/ && systemctl reload haproxy
 
-Disable local document roots (performed on esg-ansible server)
+6) Disable local document roots (performed on esg-ansible server)
 ansible-playbook <CLUSTER PREFIX>_prod /playbooks/local-docroot.yml -t disable
 
-Run any applicable playbooks from ansible server
-
-HAProxy:
+7) Run any applicable playbooks from ansible server
+- HAProxy:
 ansible-playbook <CLUSTER PREFIX>_prod /playbooks/haproxy.yml
-
-Varnish:
+- Varnish:
 ansible-playbook <CLUSTER PREFIX>_prod /playbooks/varnish.yml
-
-Local docroots:
+- Local docroots: Run in tmux or screen 
 ansible-playbook <CLUSTER PREFIX>_prod /playbooks/local-docroot.yml
-
-Redis: (skip is no replicas is being used)
-stop each instances on the web nodes (example for 2 instances,update to reflect for both)
-systemctl stop redis-multi-<REDIS_INSTANCE1>-replica.service
-
-systemctl stop redis-multi-<REDIS_INSTANCE2>-replica.service
-
-systemctl status redis-multi-<REDIS_INSTANCE1>-replica.service
-
-systemctl status redis-multi-<REDIS_INSTANCE2>-replica.service
-
-and then Reset from the Master instance (<CLUSTER PREFIX>-fs1)
+- Redis: stop each instances on the web nodes
+<REDIS_STOP_BLOCK>
+<REDIS_STATUS_BLOCK>
+- Reset from the Master instance (<CLUSTER PREFIX>-fs1)
 (connect to SENTINEL)
 redis-cli -h <INTERNAL_IP_FS1> -p 5000
 
 rest each instance:
-SENTINEL RESET <REDIS_INSTANCE1>-cache 
+<REDIS_RESET_BLOCK>
 
-SENTINEL RESET <REDIS_INSTANCE2>-cache
-
-Enable local docroots on <CLUSTER PREFIX> cluster(performed on esg-ansible server)
+8) Enable local docroots on <CLUSTER PREFIX> cluster(performed on esg-ansible server)
+- This can take a bit so run it in a screen or tmux.
 ansible-playbook <CLUSTER PREFIX>_prod /playbooks/local-docroot.yml -t enable
 
-!! THIS IS THE MOST IMPORTANT STEP !! Drop the nodes from Interworx (performed within Nodeworx panel on cluster's lb)
-
-Get Nodeworx URL:
+9) !! THIS IS THE MOST IMPORTANT STEP !! Drop the nodes from Interworx (performed within Nodeworx panel on cluster's lb)
+- Access Nodeworx URL:
 me <CLUSTER PREFIX>-lb1.us-midwest-1.nxcli.net
-
 nw
-Open Link and navigate:
-Access "Clustering" -> "Nodes"
-Select associated nodes being removed (select node# and node#)
-"With Selected: Delete"
+- Go to "Clustering" -> "Nodes"
+- Select associated nodes being removed:
+<NODE_INT_LIST>
+- "With Selected: Delete"
 
 10) Allow access from your IP addresss and perform simple testing on the site
 me <CLUSTER PREFIX>-lb1.us-midwest-1.nxcli.net 
-
 vim /etc/haproxy/maps/whitelist-ips.map
-
 haproxy -c -V -f /etc/haproxy/conf/ -f /etc/haproxy/conf.d/ && systemctl reload haproxy
-
-This is to ensure that nothing appears obviously broken
-
-Load sites in browser and navigate.
+- This is to ensure that nothing appears obviously broken
+- Load sites in browser and navigate. 
 <DOMAINS>
+
 11)  Verify that no traffic is routing to the old nodes
-Should not have any data, only this should be getting traffic:
+- Should not have any data, only this should be getting traffic:
 tail -f /var/log/interworx/<INTERNAL_IP_NODES_REMOVED>/*/logs/transfer.log
 EG: tail -f /var/log/interworx/172.18.124.16[3-4]/*/logs/transfer.log
 
-12) Assist the client in testing the site and update ticket (may not be needed if client is not available and we confirmed sites are loading from other nodes)
-may need to whitelist their IP(s), see step 10)
+12) Assist the client in testing the site and update ticket
+- may need to whitelist their IP(s), see step 10)
 me <CLUSTER PREFIX>-lb1.us-midwest-1.nxcli.net 
-
 vim /etc/haproxy/maps/whitelist-ips.map
-
-13) Remove maintenance:
-me <CLUSTER PREFIX>-lb1.us-midwest-1.nxcli.net
-vim /etc/haproxy/lists/maintenance-domains.txt and remove/comment out:
-
-<DOMAINS>
 haproxy -c -V -f /etc/haproxy/conf/ -f /etc/haproxy/conf.d/ && systemctl reload haproxy
 
-14) update OCC team in #nw-watchers
-Please resume alerts for <CLUSTER PREFIX>-* and scheduled maintenance is now completed. node# and node# have been removed from the cluster
-15) Reach out to Sales to adjust invoicing and for them to work with DCops on decommissioning the <CLUSTER PREFIX>-node# and <CLUSTER PREFIX>-node# servers.
+13) Remove maintenance
+me <CLUSTER PREFIX>-lb1.us-midwest-1.nxcli.net 
+vim /etc/haproxy/lists/maintenance-domains.txt 
+- remove/comment out:
+<DOMAINS>
+- Haproxy syntax check and reload 
+haproxy -c -V -f /etc/haproxy/conf/ -f /etc/haproxy/conf.d/ && systemctl reload haproxy
+
+14) Update OCC team in #nw-watchers
+Please resume alerts for <CLUSTER PREFIX>-* and scheduled maintenance is now completed. <NODE_COMMA_LIST> have been removed from the cluster
+
+15) Re-enable Status Cake alert 
+https://app.statuscake.com/Login/?redirect=/YourStatus2.php
+- Unpause Statuscake monitoring for the server(s) being removed
+Search for the hostnames
+Hit the pause button (Verify test is UP and not paused)
+
+16) Pass back to ESG Projects for decommissioning <NODE_COMMA_LIST>
 EOF
 )
 
-# Perform Replacements
-# Note: Using perl for the domain replacement to handle the newlines in $DOMAIN_LIST cleanly
+# Final Output with Multi-Line Replacements
 echo "$TEMPLATE" | sed \
     -e "s/<CLUSTER PREFIX>/$CLUSTER_PREFIX/g" \
-    -e "s/<SERVER LIST>/$SERVER_LIST/g" \
     -e "s/<CASE ID>/$CASE_ID/g" \
-    -e "s/<CLIENT ID>/$CLIENT_ID/g" \
-    -e "s/<REDIS_INSTANCE1>/$REDIS_INSTANCE1/g" \
-    -e "s/<REDIS_INSTANCE2>/$REDIS_INSTANCE2/g" \
+    -e "s/<CLIENT_TEAM>/$CLIENT_TEAM/g" \
     -e "s/<INTERNAL_IP_FS1>/$INTERNAL_IP_FS1/g" \
     -e "s/<INTERNAL_IP_NODES_REMOVED>/$INTERNAL_IP_NODES_REMOVED/g" | \
-    perl -pe "s/<DOMAINS>/$DOMAIN_LIST/g"
+    perl -pe "s|<NODE_BULLET_LIST>|$NODE_BULLET_LIST|g; \
+              s|<NODE_COMMA_LIST>|$NODE_COMMA_LIST|g; \
+              s|<NODE_BLACKLIST>|$NODE_BLACKLIST|g; \
+              s|<NODE_INT_LIST>|$NODE_INT_LIST|g; \
+              s|<REDIS_STOP_BLOCK>|$REDIS_STOP_BLOCK|g; \
+              s|<REDIS_STATUS_BLOCK>|$REDIS_STATUS_BLOCK|g; \
+              s|<REDIS_RESET_BLOCK>|$REDIS_RESET_BLOCK|g; \
+              s|<DOMAINS>|$DOMAIN_LIST|g"
